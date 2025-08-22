fbffb2285c9ae25f2fdcb25ae79deb5448ec7c16
Thilagavathi94 <itthilaga1@gmail.com>
2025-08-23 00:52:56 +0530

    file

---
+++ src/test/java/com/example/itsolution/ItsolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
